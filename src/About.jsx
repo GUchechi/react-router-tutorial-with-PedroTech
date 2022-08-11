1f7e84e1541f9c3d91f081edb309668bd0231937
GUchechi <godswilluchendu7@gmail.com>
2022-08-11 09:02:23 +0100

    added props

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const About = () => {
+const About = ({name, number}) => {
   return (
     <div className='about'>
-        <h1>About Page</h1>
+        <h1>{name}</h1>
+        <h1>{number}</h1>
     </div>
   )
 }
