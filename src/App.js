a8448ebbe8260328e5bbef127ace6202ec9d4b1d
GUchechi <godswilluchendu7@gmail.com>
2022-08-11 14:09:02 +0100

    wrapped my app into a browser router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './About';
 import './App.css';
 import Contact from './Contact';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={About} />
+      <Route exact path="/" component={Home} />
+        <Route path="/" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
     </Router>
   );
